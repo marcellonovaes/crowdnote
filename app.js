974a8ee00957fba22d8f1758e511b52b9f427edc
Marcello Novaes de Amorim <novaes@inf.ufes.br>
2017-10-02 14:37:15 +0000

    starting aggregation p_r

--- app.js
+++ app.js
@@ -179,8 +179,74 @@ app.post('/store', function(req, res) {
 	res.end();
 });
 
-//Group by duplicated
-app.get('/aggregate_d', function(req, res) {
+//Group by point
+//Aggregate Duplicated
+app.get('/aggregate_p_d', function(req, res) {
+	var cur= '';
+	var grp=0;
+	var groups = new Array();
+        Output.find({},function (err, C) {
+                if (err) return console.error(err);
+                for(var i=0; i < C.length; i++){
+			if( levenshtein(C[i].point,cur) >= 2 ){
+				grp++;
+				cur = C[i].point;
+				groups[grp] = new Array();
+			}
+			if(groups[grp]) groups[grp].push(C[i]);
+                }
+		for(var i=1; i<groups.length; i++){
+			var group = groups[i];
+			group.sort(function(a, b){ return levenshtein(a.content_type,b.content_type); });
+			var types = new Array();
+			var cur_type = '';
+			var type_grp = 0;
+	                for(var j=0; j < group.length; j++){
+				var l = levenshtein(group[j].content_type,cur_type);
+       	                	if( l >= 2 || l <= -2){
+                                	type_grp++;
+                                	cur_type = group[j].content_type;
+                                	types[type_grp] = new Array();
+                        	}
+                        	if(types[type_grp]) types[type_grp].push(group[j]);
+                	} 
+	                for(var j=0; j < types.length; j++){
+				var type = types[j];
+				if(type){ 
+					if(type[0].content_type!='image'){
+						type.sort(function(a, b){ return levenshtein(a.content,b.content) });
+						var cur_content = '';
+						for(k=0; k < type.length; k++){
+							var duplicated = true;
+							var l = levenshtein(cur_content,type[k].content);
+							if(l >= 2 ||l <= -2 ){
+								duplicated = false;
+								cur_content = type[k].content;
+								var mode = type[k];
+                        					var data ={'item_id':mode.item_id, 'point':mode.point, 'content_type':mode.content_type, 'content':mode.content,'uri': mode.uri,'start': mode.start,'end': mode.end,'instant': mode.instant,'point': mode.point}
+                        					var a = new Aggregation(data);
+                        					a.save(function (err, m0) {if (err) return console.error(err);});
+							}
+						}
+					}else{
+						for(k=0; k < type.length; k++){
+							var mode = type[k];
+                        				var data ={'item_id':mode.item_id, 'point':mode.point, 'content_type':mode.content_type, 'content':mode.content,'uri': mode.uri,'start': mode.start,'end': mode.end,'instant': mode.instant,'point': mode.point}
+                        				var a = new Aggregation(data);
+                        				a.save(function (err, m0) {if (err) return console.error(err);});
+						}
+					}
+				}
+			}
+		}
+		res.end();
+        }).sort({'point' : 1});
+});
+
+
+//Group by item Point
+//Aggregate by ranking
+app.get('/aggregate_p_r', function(req, res) {
 	var cur= '';
 	var grp=0;
 	var groups = new Array();
